5d356301e8aea174e560bbef696e109dbd4dd053
denisstuenko <stuenkden@gmail.com>
2023-09-21 11:13:05 +0300

    layout of rating page

--- src/components/RatingPage/index.tsx
+++ src/components/RatingPage/index.tsx
@@ -5,5 +5,50 @@ import { defaultAvatar } from "../../consts/defaultAvatar";
 import { useRating } from "../../hooks/rating.hook";
 
 export const Rating = () => {
-    return <></>
+    const {loading,users} = useRating();
+    const dataSource = users?.map((user,i) => {
+        return {
+            key:user.id,
+            index:i + 1,
+            player:<Display style={{gap:'10px'}}>
+                <Avatar src={user.photoURL || defaultAvatar}/>
+                <UserNameLink to={'/users/'+user.id}>{user.displayName}</UserNameLink>
+            </Display>,
+            numberOfMatches:user.numberOfMatches,
+            numberOfWins: user.numberOfWins,
+            rating: user.rating
+        }
+    });
+      
+    const columns = [
+        {
+            title: '№',
+            dataIndex: 'index',
+            key: 'index',
+        },
+        {
+            title: 'Player',
+            dataIndex: 'player',
+            key: 'player',
+        },
+        {
+            title: 'Matches',
+            dataIndex: 'numberOfMatches',
+            key: 'numberOfMatches',
+        },
+        {
+            title: 'Wins',
+            dataIndex: 'numberOfWins',
+            key: 'numberOfWins',
+        },
+        {
+            title: 'Rating',
+            dataIndex: 'rating',
+            key: 'rating',
+        },
+    ];
+      
+    return <Display style={{width:'520px',height:'100%',padding:'20px 0 0 20px',overflow:'scroll'}}>
+        <Table pagination={false} loading={loading} dataSource={dataSource} columns={columns}/>
+    </Display>
 }
