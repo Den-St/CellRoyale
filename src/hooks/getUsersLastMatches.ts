4d060ff9bee54bc6957621b62aafb6dfc557ec87
denisstuenko <stuenkden@gmail.com>
2023-09-29 15:46:48 +0300

    fixed user gets his matches at other user page

--- src/hooks/getUsersLastMatches.ts
+++ src/hooks/getUsersLastMatches.ts
@@ -1,29 +1,32 @@
+import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getLastMatches } from '../firebase/db/matchResults/get/getLastMatches';
 import { MatchInfoT } from '../types/matchInfo';
 import { useAppSelector } from './redux';
 
 export const useGetMyLastMatches = () => {
-    const userId = useAppSelector(state => state.user.id);
+    const myId = useAppSelector(state => state.user.id);
     const [loading,setLoading] = useState(false);
     const [matches,setMatches] = useState<MatchInfoT[]>();
+    const userId = useParams().id;
 
     const fetch = async () => {
-        if(!userId) return;
+        if(!myId) return;
         setLoading(true);
-        const res = await getLastMatches(userId);
+        const res = await getLastMatches(userId || myId);
         if(!res) return;
         setMatches(res.map(res => ({
             id:res.id,
             createdAt:res.createdAt,
-            place:res.playersPlaces.find(playerPlace => playerPlace.player === userId)?.place || 0,
+            place:res.playersPlaces.find(playerPlace => playerPlace.player === userId || playerPlace.player === myId)?.place || 0,
         })));
         setLoading(false);
     }
 
     useEffect(() => {
         fetch();
-    },[userId]);
+    },[myId,userId]);
 
     return {loading,matches};
 }
+
